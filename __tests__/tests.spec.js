0cde91e5a897ad961e24278619f79ef6a97da438
Zsákai Bálint <ruphyy@inf.elte.hu>
2022-08-09 15:33:48 +0200

    added chessBoardStartingSetup function + test

--- __tests__/tests.spec.js
+++ __tests__/tests.spec.js
@@ -1,12 +1,19 @@
 var functions = require("../functions"); 
 
 describe("Testing", () => {
-    test("addTwo function test", () => {
-      const input = 1 + 2;
-    
-      const output = functions.addTwo(1,2);
-  
-      expect(input).toEqual(output);
-  
+    test("chessBoardStartingSetup", () => {
+      var jsonObj1 = functions.chessBoardStartingSetup();
+      var jsonObj2 = [];
+
+      var colToPiece = new Map([['a','rook'],['b','knight'],['c','bishop'],['d','queen'],['e','king'],['f','bishop'],['g','knight'],['h','rook']]);
+      for(let i=0;i<8;++i) {
+        var column = String.fromCharCode(97 + i); //a,b,c,d,e,f,g,h
+        jsonObj2.push({ location: {row: 1, col: column}, pieceType: colToPiece.get(column), isplayerPiece: true, isInGame: true });
+        jsonObj2.push({ location: {row: 2, col: column}, pieceType: "pawn", isplayerPiece: true, isInGame: true });
+        jsonObj2.push({ location: {row: 7, col: column}, pieceType: "pawn", isplayerPiece: false, isInGame: true });
+        jsonObj2.push({ location: {row: 8, col: column}, pieceType: colToPiece.get(column), isplayerPiece: false, isInGame: true });
+      }
+
+      expect(jsonObj1).toEqual(jsonObj2);
     });
-  });
+});
--- functions.js
+++ functions.js
@@ -1,15 +1,44 @@
 //function definitions
-function addTwo(a,b) {
-    return a + b;
-}
 
-function lolSomething() {
-    return 42;
-}
+function chessBoardStartingSetup() {
+    var setup = [
+        { location: {row: 1, col: "a"}, pieceType: "rook", isplayerPiece: true, isInGame: true },
+        { location: {row: 2, col: "a"}, pieceType: "pawn", isplayerPiece: true, isInGame: true },
+        { location: {row: 7, col: "a"}, pieceType: "pawn", isplayerPiece: false, isInGame: true },
+        { location: {row: 8, col: "a"}, pieceType: "rook", isplayerPiece: false, isInGame: true },
+        { location: {row: 1, col: "b"}, pieceType: "knight", isplayerPiece: true, isInGame: true },
+        { location: {row: 2, col: "b"}, pieceType: "pawn", isplayerPiece: true, isInGame: true },
+        { location: {row: 7, col: "b"}, pieceType: "pawn", isplayerPiece: false, isInGame: true },
+        { location: {row: 8, col: "b"}, pieceType: "knight", isplayerPiece: false, isInGame: true },
+        { location: {row: 1, col: "c"}, pieceType: "bishop", isplayerPiece: true, isInGame: true },
+        { location: {row: 2, col: "c"}, pieceType: "pawn", isplayerPiece: true, isInGame: true },
+        { location: {row: 7, col: "c"}, pieceType: "pawn", isplayerPiece: false, isInGame: true },
+        { location: {row: 8, col: "c"}, pieceType: "bishop", isplayerPiece: false, isInGame: true },
+        { location: {row: 1, col: "d"}, pieceType: "queen", isplayerPiece: true, isInGame: true },
+        { location: {row: 2, col: "d"}, pieceType: "pawn", isplayerPiece: true, isInGame: true },
+        { location: {row: 7, col: "d"}, pieceType: "pawn", isplayerPiece: false, isInGame: true },
+        { location: {row: 8, col: "d"}, pieceType: "queen", isplayerPiece: false, isInGame: true },
+        { location: {row: 1, col: "e"}, pieceType: "king", isplayerPiece: true, isInGame: true },
+        { location: {row: 2, col: "e"}, pieceType: "pawn", isplayerPiece: true, isInGame: true },
+        { location: {row: 7, col: "e"}, pieceType: "pawn", isplayerPiece: false, isInGame: true },
+        { location: {row: 8, col: "e"}, pieceType: "king", isplayerPiece: false, isInGame: true },
+        { location: {row: 1, col: "f"}, pieceType: "bishop", isplayerPiece: true, isInGame: true },
+        { location: {row: 2, col: "f"}, pieceType: "pawn", isplayerPiece: true, isInGame: true },
+        { location: {row: 7, col: "f"}, pieceType: "pawn", isplayerPiece: false, isInGame: true },
+        { location: {row: 8, col: "f"}, pieceType: "bishop", isplayerPiece: false, isInGame: true },
+        { location: {row: 1, col: "g"}, pieceType: "knight", isplayerPiece: true, isInGame: true },
+        { location: {row: 2, col: "g"}, pieceType: "pawn", isplayerPiece: true, isInGame: true },
+        { location: {row: 7, col: "g"}, pieceType: "pawn", isplayerPiece: false, isInGame: true },
+        { location: {row: 8, col: "g"}, pieceType: "knight", isplayerPiece: false, isInGame: true },
+        { location: {row: 1, col: "h"}, pieceType: "rook", isplayerPiece: true, isInGame: true },
+        { location: {row: 2, col: "h"}, pieceType: "pawn", isplayerPiece: true, isInGame: true },
+        { location: {row: 7, col: "h"}, pieceType: "pawn", isplayerPiece: false, isInGame: true },
+        { location: {row: 8, col: "h"}, pieceType: "rook", isplayerPiece: false, isInGame: true }
+    ];
+    return setup;
+};
 
-//we specify here that we want to export these functions from the
-//functions.js module
+//we specify here that we want to export these functions from the functions.js module
 module.exports = {
-    addTwo,
-    lolSomething
+    chessBoardStartingSetup
 }
